64e0468e073d2b7e10f2b08401fa3ce9da7044b5
Александр Пинчук <AlexanderPinchuk1@gmail.com>
2021-06-16 16:00:23 +0300

    Remove merge conflict

--- iTechArt.Survey.Domain/Identity/User.cs
+++ iTechArt.Survey.Domain/Identity/User.cs
@@ -6,10 +6,7 @@ namespace iTechArt.Survey.Domain.Identity
     public class User : IdentityUser<Guid>
     {
         public string DisplayName { get; set; }
-<<<<<<< HEAD
 
-=======
->>>>>>> origin/main
         public DateTime RegistrationDateTime { get; set; }
     }
 }
